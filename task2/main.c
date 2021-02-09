803e1840fc08be34c42138198c7f1fa655506786
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2021-02-09 10:08:50 +0700

    Update main.c

--- task2/main.c
+++ task2/main.c
@@ -7,14 +7,14 @@ extern char *tzname[];
 
 int main() {
     time_t now;
-    struct tm *sp;
+    struct tm *time;
     putenv ("TZ=America/Los_Angeles");
     (void) time(&now);
     printf("%s", ctime(&now));
-    sp = localtime(&now);
+    time = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n",
-           sp->tm_mon + 1, sp->tm_mday, sp->tm_year,
-           sp->tm_hour, sp->tm_min,
+           time->tm_mon + 1, time->tm_mday, time->tm_year,
+           time->tm_hour, time->tm_min,
            tzname[sp->tm_isdst]);
     exit(0);
 }
