f50dd3a29d5331655beb838d8cbd2b088634cb46
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2021-02-27 17:46:26 +0700

    Update main.c

--- task3/main.c
+++ task3/main.c
@@ -1,31 +1,21 @@
 #include <sys/types.h>
-//файл библиотеки. В этом файле объявляются системные типы данных, такие как u_int, u_char... В нашем случае нужен для uid_t -- user IDs.
 #include <unistd.h>
-//unistd.h -- заголовочный файл объявляет ряд специфичных для реализации функций: 	getuid(), 	geteuid(), setuid()...
 #include <stdio.h>
-//заголовочный файл ввода-вывода.
 #define ERROR -1
-//Директива #define определяет идентификатор и последовательность символов, которые будут подставляться вместо идентификатора каждый раз, когда он встретится
 
 int main(int argc, char *argv[]) {
     printf("\nReal user ID: %d\n", getuid());
-    /*Функция printf() записывает в stdout строку "Real user ID:" и значение, полученное функцией getuid().
-    %d -- спецификатор формата (десятичного числа целого типа со знаком).
-    getuid() возвращает фактический идентификатор (ID) пользователя вызывающего процесса.
-    geteuid() возвращает эффективный идентификатор (ID) пользователя в текущем процессе.
-     */
     printf("Effective user ID: %d\n", geteuid());
     FILE *file1 = fopen("file1.txt", "r");
-    //создание объекта типа FILE, открытие файла под названием file1.txt для чтения
-    if (file1 == NULL) { // проверка на успешное открытие файла
-        perror ("The file cannot be opened"); // Функция perror() выводит сообщение об ошибке в stderr
+    if (file1 == NULL) { 
+        perror ("The file cannot be opened"); 
     } else {
         printf("The file opened successfully\n");
-        fclose(file1); //при успешном открытии файла выводим в stdout строку "The file opened successfully\n" и закрываем файл, с которым работали
+        fclose(file1); 
     }
     int checkID = seteuid(getuid());
-    //  seteuid -- установка идентификаторов пользователя
-    if (checkID == ERROR) { //если функция выполнена с ошибкой
+    
+    if (checkID == ERROR) { 
         perror("This id cannot be set");
     }
     printf("\nReal user ID: %d\n", getuid());
