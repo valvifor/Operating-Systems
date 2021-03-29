b1e1caa8eb329657493430b8fa62036a3f4958bd
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2021-03-29 23:18:50 +0700

    Add files via upload

---
+++ lab10/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <wait.h>
+
+#define WRONG_ARGC 1
+#define EXIT_CODE 1
+#define ERROR -1
+#define ERROR_WIFEXITED 0
+#define SUCCESS 0
+
+int main(int argc, char * argv[]){
+    pid_t pid;// pid_t тип данных для ID процесса
+    int status;
+    if(argc == WRONG_ARGC) {
+        printf("No program availiable\n");
+        exit(EXIT_CODE);
+    }
+    pid = fork();// создать новый прооцесс. Родительский процесс -> идентификатор порожденного
+    //процесса. Проржденный процесс -> 0. Неудача -> -1 и устанавливается значение errno
+    if (pid == ERROR) {
+        perror("Fork error");
+        exit(EXIT_CODE);
+    } else if (pid == SUCCESS) {// порожденный процесс
+        char *command = argv[1];
+        int callingCode = execvp(command, argv + 1);
+        if (callingCode == ERROR) {
+            perror("Execvp error");
+            exit(EXIT_CODE);
+        }
+    }
+    int exitCode;
+    pid_t childID = wait(&exitCode);//ожидание остановки или завершения порожденного процесса
+    int check = WIFEXITED(exitCode);//не равно нулю, если дочерний процесс успешно завершился.
+    if(check != ERROR_WIFEXITED) {
+        int exitStatus = WEXITSTATUS(status);
+        printf("\nExit code %lld: %d\n", pid, exitStatus);
+    } else {
+        printf("\nthe child process %lld failed with an error\n", pid);
+        exit(EXIT_CODE);
+    }
+    return 0;
+}
