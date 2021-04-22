22cb27f0f76124c1307dc1148698eedb3bce2dfd
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2021-04-22 20:19:46 +0700

    Add files via upload

---
+++ lab18/cmake-build-debug/CMakeFiles/lab18.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab18.dir/main.c.obj"
+  "lab18.exe"
+  "lab18.exe.manifest"
+  "lab18.pdb"
+  "liblab18.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab18.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab18/main.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+
+#define SIZE_BUF 200
+#define TIME_LENGTH 200
+#define MODE_LENGTH 11
+#define ERROR -1
+#define CURRENT "."
+#define NUM_OF_ARGS 1
+#define SUCCESS 0
+
+void getMode(struct stat stat) {
+    char mode[MODE_LENGTH] = { 0 };
+    switch (stat.st_mode & S_IFMT) {
+        case S_IFDIR: mode[0] = 'd'; break;
+        case S_IFREG: mode[0] = '-'; break;
+        default: mode[0] = '?';
+    }
+    //user
+    mode[1] = (stat.st_mode & S_IRUSR) ? 'r' : '-';
+    mode[2] = (stat.st_mode & S_IWUSR) ? 'w' : '-';
+    mode[3] = (stat.st_mode & S_IXUSR) ? 'x' : '-';
+    //group
+    mode[4] = (stat.st_mode & S_IRGRP) ? 'r' : '-';
+    mode[5] = (stat.st_mode & S_IWGRP) ? 'w' : '-';
+    mode[6] = (stat.st_mode & S_IXGRP) ? 'x' : '-';
+    //other
+    mode[7] = (stat.st_mode & S_IROTH) ? 'r' : '-';
+    mode[8] = (stat.st_mode & S_IWOTH) ? 'w' : '-';
+    mode[9] = (stat.st_mode & S_IXOTH) ? 'x' : '-';
+    mode[10] = '\0';
+    printf("%s", mode);
+}
+
+void getNumberLinks(struct stat stat) {
+    printf("%4d", stat.st_nlink);
+}
+
+void getOwner(struct stat stat) {
+    struct passwd* pwd;
+    pwd = getpwuid(stat.st_uid);
+    if (pwd == NULL) {
+        printf(" %d", stat.st_uid);
+    } else {
+        printf(" %s", pwd->pw_name);
+    }
+}
+
+void getGroup(struct stat stat) {
+    struct group* grp;
+    grp = getgrgid(stat.st_gid);
+    if (grp == NULL) {
+        printf(" %d", stat.st_gid);
+    } else {
+        printf(" %s", grp->gr_name);
+    }
+}
+
+void getFileSize(struct stat stat){
+    printf("    %ld", stat.st_size);
+}
+
+void getTime(struct stat stat) {
+    char lbuf[SIZE_BUF] = { 0 };
+    strftime(lbuf, TIME_LENGTH, "%b %e %H:%M", localtime(&(stat.st_ctime)));
+    printf(" %s", lbuf);
+}
+
+void getFileName(char** dirs, int i) {
+    printf(" %s\n", dirs[i]);
+}
+
+int lsCommand(char** dirs) {
+    struct stat stat;
+// struct stat {
+//     mode_t        st_mode;     /* тип файла и режим (права доступа) */
+//     ino_t         st_ino;      /* (inode) номер индексного узла */
+//     dev_t         st_dev;      /* номер устройства (файловой системы) */
+//     dev_t         st_rdev;     /* номер устройства для спец файлов */
+//     nlink_t       st_nlink;    /* количество ссылок */
+//     uid_t         st_uid;      /* идентификатор пользователя владельца */
+//     gid_t         st_gid;      /* идентификатор группы владельца */
+//     off_t         st_size;     /* размер в байтах, для обычного файла*/
+//     time_t        st_atime;    /* время последнего обращения к файлу */
+//     time_t        st_mtime;    /* время последнего изменения файла */
+//     time_t        st_ctime;    /* время последнего изменения состояния файла */
+//     blksize_t     st_blksize;  /* оптимальный размер блока ввода-вывода */
+//     blkcnt_t      st_blocks;   /* количество занятых дисковых блоков */
+// };
+
+    for (int i = 0; dirs[i] != NULL; ++i) {
+        if (lstat(dirs[i], &stat)) {
+            perror("lstat() failed");
+            return ERROR;
+        }
+        getMode(stat);
+        getNumberLinks(stat);
+        getOwner(stat);
+        getGroup(stat);
+        getFileSize(stat);
+        getTime(stat);
+        getFileName(dirs, i);
+    }
+    return SUCCESS;
+}
+
+int main (int argc, char* argv[]) {
+    char* args[] = {CURRENT, NULL};
+    char** dirs = NULL;
+    if (argc > NUM_OF_ARGS) {
+        dirs = argv + 1;
+    } else {
+        dirs = args;
+    }
+    if (lsCommand(dirs) == ERROR) {
+        return ERROR;
+    }
+    return SUCCESS;
+}
