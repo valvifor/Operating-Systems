9d5f851086aef8a8d53217a918b2316c6491d5a4
ValeriaPetrova <73832354+ValeriaPetrova@users.noreply.github.com>
2021-05-03 19:16:56 +0700

    Add files via upload

---
+++ lab20/cmake-build-debug/CMakeFiles/lab20.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab20.dir/main.c.obj"
+  "lab20.exe"
+  "lab20.exe.manifest"
+  "lab20.pdb"
+  "liblab20.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab20/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <limits.h>
+#include <glob.h>
+
+#define SIZE 256
+#define NOT_END_OF_INPUT 1
+#define ERROR -1
+#define SUCCESS 0
+
+int readPattern(char *pattern) {
+    printf("Enter pattern: ");
+    int patternLength = 0;
+    while(NOT_END_OF_INPUT) {
+        char currentSymbol = (char)fgetc(stdin);
+        if (currentSymbol == EOF || currentSymbol == '\n') {
+            break;
+        }
+        pattern[patternLength] = currentSymbol;
+        patternLength++;
+    }
+    if (patternLength == 0) {
+        fprintf(stderr, "the pattern cannot be empty\n");
+        return ERROR;
+    }
+    pattern[patternLength] = '\0';
+    return EXIT_SUCCESS;
+}
+
+int print(char *pattern) {
+    glob_t result;
+    int flag = glob(pattern, GLOB_NOCHECK, NULL, &result);
+    if (flag != SUCCESS) {
+        perror("glob error");
+        globfree(&result);
+        return ERROR;
+    }
+    for (int i = 0; i < result.gl_pathc; i++) {
+        printf("%s\n", result.gl_pathv[i]);
+    }
+    globfree(&result);
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char* argv[]) {
+    char pattern[SIZE];
+    if (readPattern(pattern) == ERROR) {
+        exit(EXIT_FAILURE);
+    }
+    if (print(pattern) == ERROR) {
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
+}
